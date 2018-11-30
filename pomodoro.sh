ab617f7b0748a4f827de71bfbcac713dffca8d05
p.a.zhdanov <p.a.zhdanov@mai.ru>
2018-11-30 17:58:48 +0300

    Initial commit. Shell pomodoro timer with notifications.

---
+++ pomodoro.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is a simple pomodoro script
+# Inspired by https://superuser.com/a/669811
+
+icon_path="" 
+
+notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nStart"
+
+while true; do
+	sleep 1500 && notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nShort break"
+	sleep  300 && notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nBack to work"
+
+	sleep 1500 && notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nShort break"
+	sleep  300 && notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nBack to work"
+
+	sleep 1500 && notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nShort break"
+	sleep  300 && notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nBack to work"
+
+	sleep 1500 && notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nlong break"
+	sleep  300 && notify-send -u critical -t 0 -i $icon_path "Tomato" "`date`\nBack to work"
+done
